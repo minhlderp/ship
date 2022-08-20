54a9e5904b589fb3ce218aa6a8ee64ae6e0c19b7
minhld <minh1802.ld@gmail.com>
2022-08-20 11:40:47 +0700

    'fix'

--- ship_pricelist/models/service_type.py
+++ ship_pricelist/models/service_type.py
@@ -8,12 +8,9 @@ class ServiceType(models.Model):
     name = fields.Char(string=_('Name'), required=1)
     active = fields.Boolean(string=_('Active'), default=True)
     price_list = fields.Boolean('Bảng giá thu hộ')
-<<<<<<< HEAD
 
 class YearApply(models.Model):
     _name = 'year.apply'
 
 
     name = fields.Char('Year apply')
-=======
->>>>>>> 131a8e3bf586708491630fa78f85c10c67411111
