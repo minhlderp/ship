622e5f6586dd64757e2e6c473f8e4b8144c4a70e
hieukun1011 <hieukun1011@gmail.com>
2022-08-22 09:34:30 +0700

    update

--- ship_pricelist/models/ship_pricelist.py
+++ ship_pricelist/models/ship_pricelist.py
@@ -1,7 +1,6 @@
 from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
 
-
 class ShipPricelist(models.Model):
     _name = 'ship.pricelist'
     _description = 'Ship pricelist'
@@ -12,7 +11,7 @@ class ShipPricelist(models.Model):
     to_locations = fields.Many2many('res.country.state', string=_('To Locations'), required=1)
     fuel_fee = fields.Float(string=_('Fuel Fee'), digits='Product Unit of Measure')
     region_fee = fields.Float(string=_('Region Fee'), digits='Product Unit of Measure')
-    year_apply = fields.Char(string=_('Year apply'))
+    year_apply = fields.Many2one('year.apply', string=_('Year apply'))
     over_weight = fields.Float(string=_('Over Weight (Gr)'), digits='Product Unit of Measure')
     over_weight_fee = fields.Float(string=_('Over Weight Fee'), digits='Product Unit of Measure')
     line_ids = fields.One2many('ship.pricelist.line', 'ship_pricelist_id', string=_('Pricelist Lines'))
@@ -34,3 +33,27 @@ class ShipPricelist(models.Model):
                 error.append(line)
         if error:
             raise ValidationError(_("Ship pricelist line '%s': Weight range cannot be overlapped", ', '.join(e.name for e in error)))
+
+    @api.constrains('surcharge_ids')
+    def validate_range_from_to_weight_surcharge(self):
+        error = []
+        for line in self.surcharge_ids:
+            exist = self.surcharge_ids.filtered(lambda l: l.id != line.id and (
+                    l.price <= line.price))
+            if exist:
+                error.append(line)
+        if error:
+            raise ValidationError(
+                _("Surcharge line '%s': Weight range cannot be overlapped", ', '.join(e.name for e in error)))
+
+    @api.constrains('fees_collection_ids')
+    def validate_range_from_to_weight_fees_collection(self):
+        error = []
+        for line in self.fees_collection_ids:
+            exist = self.fees_collection_ids.filtered(lambda l: l.id != line.id and (
+                    l.from_weight <= line.from_weight <= l.to_weight or l.from_weight <= line.to_weight <= l.to_weight))
+            if exist:
+                error.append(line)
+        if error:
+            raise ValidationError(
+                _("Fees collection line '%s': Weight range cannot be overlapped", ', '.join(e.name for e in error)))
